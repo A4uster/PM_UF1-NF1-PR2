6a0d0b3a8d971ef34d6b58ec79ac3c3373869a30
Damian <tdemian.09112@gmail.com>
2023-02-15 20:01:33 +0100

    Creacion de app Autor @A4uster 1. Crear dos layouts 2. Crear dos fragments que aparezcan en los diferentes activitys 3. Pasar datos de un Activity a otro sin Model View View Model MVVM

---
+++ app/src/main/java/com/example/pm_uf1_nf1_pr2/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.pm_uf1_nf1_pr2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mET_correo, mET_password;
+    private Button mBT_Register, mBT_Login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        InicializarComponents();
+
+        mBT_Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity2.class);
+
+                intent.putExtra("correo", "Recuerda que estos datos seran tratados por firebase.");
+
+                startActivity(intent);
+
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.FL_Login, new LoginFragment()).commit();
+
+    }
+
+    private void InicializarComponents() {
+        mET_correo = findViewById(R.id.ET_ICorreo);
+        mET_password = findViewById(R.id.ET_IPassword);
+
+        mBT_Login = findViewById(R.id.BT_Login);
+        mBT_Register =findViewById(R.id.BT_Regist);
+
+    }
+}
---
+++ app/src/main/java/com/example/pm_uf1_nf1_pr2/RegisterActivity2.java
@@ -0,0 +1,57 @@
+package com.example.pm_uf1_nf1_pr2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class RegisterActivity2 extends AppCompatActivity {
+
+    private EditText mET_nombreCompleto, mET_username, mET_correos , mET_password, mET_NewPassword;
+    private Button mBT_NRegistro;
+    private FrameLayout mFL_RegistDades;
+    private TextView mTV_IExtra;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+
+        InicializarComponents();
+
+        String CorreoRecibidoSMV = getIntent().getStringExtra("correo");
+        mTV_IExtra.setText(CorreoRecibidoSMV);
+
+
+        mBT_NRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.FL_Register, new RegisterFragment()).commit();
+
+
+    }
+    private void InicializarComponents() {
+        mET_nombreCompleto = findViewById(R.id.ET_MNombre);
+        mET_username = findViewById(R.id.ET_MUsername);
+        mET_correos = findViewById(R.id.ET_MCorreo);
+        mET_password = findViewById(R.id.ET_MPassword);
+        mET_NewPassword = findViewById(R.id.ET_MnewPassword);
+
+       mBT_NRegistro = findViewById(R.id.BT_MRegist);
+
+        //mFL_RegistDades = findViewById(R.id.FL_Register);
+        mTV_IExtra = findViewById(R.id.TV_IExtraRecibido);
+
+
+    }
+}
